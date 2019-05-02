d13597359d9e573986c99093314673e520815df9
Robert Bart <rbart234@gmail.com>
2019-05-01 20:44:18 -0700

    Simplify and improve QuadTree implementation.

--- app/datastructure/QuadTreeNode.spec.ts
+++ app/datastructure/QuadTreeNode.spec.ts
@@ -1,4 +1,4 @@
-import QuadTreeLeafNode from './QuadTreeLeafNode';
+import QuadTreeLeafNode from './QuadTreeNode';
 import Vector2d from '../state/Vector2d'
 import HasPosition2d from '../state/HasPosition2d'
 import { expect } from 'chai';
@@ -26,7 +26,7 @@ describe('QuadTreeLeafNode', () => {
     let node = new QuadTreeLeafNode<TestElement>(origin, extents);
     let testElement = new TestElement(2, 2)
     node.add(testElement)
-    let actualElements = [...node]
+    let actualElements = node.elements!
     expect(actualElements.length).to.equal(1);
     expect(actualElements[0]).to.equal(testElement);
   });
@@ -37,7 +37,8 @@ describe('QuadTreeLeafNode', () => {
     node.add(testElement)
     node.add(testElement)
     node.add(testElement)
-    let actualElements = [...node]
+    let actualElements = node.elements!
+    expect(node.isLeaf).to.be.true
     expect(actualElements.length).to.equal(3);
     actualElements.forEach(el => expect(el).to.equal(testElement))
   });
@@ -46,10 +47,11 @@ describe('QuadTreeLeafNode', () => {
     let node = new QuadTreeLeafNode<TestElement>(origin, extents);
     let element1 = new TestElement(2, 2)
     let element2 = new TestElement(3, 3)
-    expect(node.add(element1)).to.equal(node)
-    let expected_internal = node.add(element2)
+    node.add(element1)
+    node.add(element2)
 
-    let actualElements = [...expected_internal]
+    let actualElements = node.elements!
+    expect(node.isLeaf).to.be.false
     expect(actualElements.length).to.equal(2);
     expect(actualElements[0]).to.equal(element1);
     expect(actualElements[1]).to.equal(element2);
--- app/datastructure/QuadTreeNode.ts
+++ app/datastructure/QuadTreeNode.ts
@@ -1,10 +1,18 @@
 import HasPosition2d from '../state/HasPosition2d'
 import Vector2d from '../state/Vector2d'
 
-export default abstract class QuadTreeNode<TElement extends HasPosition2d> implements Iterable<TElement> {
+export default class QuadTreeNode<TElement extends HasPosition2d> {
+
+  public elements: TElement[] | null = null
+  public upperLeft: QuadTreeNode<TElement> | null = null
+  public upperRight: QuadTreeNode<TElement> | null = null
+  public lowerLeft: QuadTreeNode<TElement> | null = null
+  public lowerRight: QuadTreeNode<TElement> | null = null
+  public isLeaf: boolean = true
+
   constructor(
-    readonly origin: Vector2d,
-    readonly extents: Vector2d) {}
+    public origin: Vector2d,
+    public extents: Vector2d) { }
 
   contains(position: Vector2d): boolean {
     return position.x >= this.origin.x &&
@@ -13,13 +21,104 @@ export default abstract class QuadTreeNode<TElement extends HasPosition2d> imple
       position.y < this.origin.y + this.extents.y;
   }
 
-  abstract add(element: TElement): QuadTreeNode<TElement>
+  add(element: TElement): void {
+    if (this.elements == null) {
+      this.elements = [element]
+    } else {
+      this.elements.push(element);
 
-  abstract [Symbol.iterator](): Iterator<TElement>
+      if (!this.allSamePosition(element) && !this.tooSmallExtents()) {
+        if (this.isLeaf) {
+          this.isLeaf = false
+          this.initializeChildren()
+          this.elements.forEach(el => this.addToChildren(el))
+        } else {
+          this.addToChildren(element)
+        }
+      }
+    }
+  }
 
-  abstract allNodes(): Iterable<QuadTreeNode<TElement>>
+  private initializeChildren(): void {
+    let halfExtent = this.extents.multiply(0.5)
+    this.upperLeft = new QuadTreeNode<TElement>(this.origin, halfExtent)
+    this.upperRight = new QuadTreeNode<TElement>(this.origin.addX(halfExtent.x), halfExtent)
+    this.lowerLeft = new QuadTreeNode<TElement>(this.origin.addY(halfExtent.y), halfExtent)
+    this.lowerRight = new QuadTreeNode<TElement>(this.origin.add(halfExtent), halfExtent)
+  }
 
-  abstract allLeafNodes(): Iterable<QuadTreeNode<TElement>>
+  private addToChildren(element: TElement) {
+    if (this.upperLeft!.contains(element.position())) {
+      this.upperLeft!.add(element);
+    }
+    else if (this.upperRight!.contains(element.position())) {
+      this.upperRight!.add(element);
+    }
+    else if (this.lowerLeft!.contains(element.position())) {
+      this.lowerLeft!.add(element);
+    }
+    else if (this.lowerRight!.contains(element.position())) {
+      this.lowerRight!.add(element);
+    }
+  }
 
-  abstract allNonIntersectingNodes(position: Vector2d): Iterable<QuadTreeNode<TElement>>
+  private allSamePosition(element: TElement): boolean {
+    return this.elements!.every(e => e.position().equals(element.position()))
+  }
+
+  private tooSmallExtents() {
+    return this.extents.length() < 10
+  }
+
+  containingNode(position: Vector2d): QuadTreeNode<TElement> | null {
+    if (this.isLeaf) {
+      return this
+    } else {
+      for (let child of this.children()) {
+        if (child.contains(position)) {
+          return child.containingNode(position)
+        }
+      }
+      return null;
+    }
+  }
+
+  allLeafNodes(): QuadTreeNode<TElement>[] {
+    if (this.isLeaf) {
+      if (this.elements != null) {
+        return [this]
+      } else {
+        return []
+      }
+    } else {
+      return this.children().map(child => child.allLeafNodes()).reduce((a,b) => a.concat(b))
+    }
+  }
+
+  allNonIntersectingNodes(position: Vector2d): QuadTreeNode<TElement>[] {
+    if (!this.contains(position) && this.elements != null && this.extents.x < 200) {
+      return [this]
+    }
+    else if (!this.isLeaf) {
+      return this.children().map(child => child.allNonIntersectingNodes(position)).reduce((a,b) => a.concat(b))
+    } else {
+      return []
+    }
+  }
+
+  private children_memo: QuadTreeNode<TElement>[] | null = null;
+  private children(): QuadTreeNode<TElement>[] {
+    if (this.children_memo == null) {
+     this.children_memo = [this.upperLeft!, this.upperRight!, this.lowerLeft!, this.lowerRight!]
+   }
+   return this.children_memo!
+  }
+
+  // abstract [Symbol.iterator](): Iterator<TElement>
+  //
+  // abstract allNodes(): Iterable<QuadTreeNode<TElement>>
+  //
+  // abstract allLeafNodes(): Iterable<QuadTreeNode<TElement>>
+  //
+  // abstract allNonIntersectingNodes(position: Vector2d): Iterable<QuadTreeNode<TElement>>
 }
