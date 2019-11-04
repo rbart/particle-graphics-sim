f16f6268deaaafdb4d529bb92a415bff3e78a81b
Robert Bart <rbart234@gmail.com>
2019-11-03 19:00:19 -0800

    Re-enable quadtree renderer

--- app/visualization/RendererBuilder.ts
+++ app/visualization/RendererBuilder.ts
@@ -1,5 +1,5 @@
 import CanvasRenderer from './CanvasRenderer'
-//import QuadTreeRenderer from './QuadTreeRenderer'
+import QuadTreeRenderer from './QuadTreeRenderer'
 import Renderer from './Renderer'
 import Particle from '../state/Particle'
 import Vector2d from '../state/Vector2d'
@@ -13,15 +13,15 @@ export default class RendererBuilder {
     fadeRate: number): Renderer {
 
     let canvasRenderer = new CanvasRenderer(ctx, width, height, fadeRate)
-    //let quadTreeRenderer = new QuadTreeRenderer(ctx, new Vector2d(width, height))
+    let quadTreeRenderer = new QuadTreeRenderer(ctx, new Vector2d(width, height))
     let renderer: Renderer = {
       initialize() {
         canvasRenderer.initialize()
-        //quadTreeRenderer.initialize()
+        quadTreeRenderer.initialize()
       },
       render(elements: Particle[]) {
         canvasRenderer.render(elements)
-        //quadTreeRenderer.render(elements)
+        quadTreeRenderer.render(elements)
       }
     }
     return renderer
